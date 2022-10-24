c0b4b12e1ee869f30e43a26b85443be2a2a631ca
BridgeQZH <zju_qzh@163.com>
2022-10-24 21:32:51 +0200

    delete test file

--- test_read.py
+++
@@ -1,8 +0,0 @@
-import csv
-csv_filename = 'CR_107_2.csv'
-with open(csv_filename, 'rt') as f:
-    reader = csv.DictReader(f)
-    # reader_dict = dict(reader)
-    # print(reader_dict)
-    for row in reader:
-        print(row)
