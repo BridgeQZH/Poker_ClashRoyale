f2ad84dc6f95257c1e032792eed70e550d9541fa
BridgeQZH <zju_qzh@163.com>
2022-12-16 15:14:13 +0100

    add pics

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from number_to_card_name import number_to_card_name
 
 if __name__ == "__main__":
     player1_hold_card, player2_hold_card, common_card_list = shuffle_and_deal()
-    print("player1_hold_card:", number_to_card_name(player1_hold_card))
-    print("player2_hold_card:", number_to_card_name(player2_hold_card))
-    print("common_card:", number_to_card_name(common_card_list))
+    print("玩家一的手牌：", number_to_card_name(player1_hold_card))
+    print("玩家二的手牌：", number_to_card_name(player2_hold_card))
+    print("公共牌：", number_to_card_name(common_card_list))
     
